39f77f8460fe2308746ff4ca92cf15aba130ce71
thetrueseros <leugimdzn@hotmail.com>
2025-10-17 14:05:05 -0500

    feat: agrega clase base abstracta Sticker

---
+++ StickerBoard/Sticker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickerBoard
+{
+    public abstract class Sticker
+    {
+        private int posX { get; set; }
+        private int posY { get; set; }
+        private int tamaño { get; set; }
+        public abstract void Dibujar( int posX, int posY, int tamaño );
+    }
+}
