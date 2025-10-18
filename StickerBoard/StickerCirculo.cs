36404999ea9628ad875fd5792813a2fb5ee0ef71
thetrueseros <leugimdzn@hotmail.com>
2025-10-18 01:39:22 -0500

    feat: implementa StickerCirculo

---
+++ StickerBoard/StickerCirculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickerBoard
+{
+    public class StickerCirculo : Sticker
+    {
+        public StickerCirculo(int posX, int posY, int tamaño, Color color) : base(posX, posY, tamaño, color)
+        {
+        }
+        public override void Dibujar(Graphics g, int posX, int posY, int tamaño, Color color)
+        {
+            //Graphics g = Graphics.FromImage(bmp); (tener en cuenta para Factory o interfaz)
+            SolidBrush b = new SolidBrush(color);
+            g.FillEllipse(b, posX, posY, tamaño, tamaño);
+            g.DrawEllipse(Pens.Black, posX, posY, tamaño, tamaño);
+            g.Dispose();
+        }
+    }
+}
