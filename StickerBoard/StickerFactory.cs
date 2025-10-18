0a7549c7953bddbf7a7172286d7e60d8f68e673a
thetrueseros <leugimdzn@hotmail.com>
2025-10-18 01:44:08 -0500

    feat: implementa StickerFactory (Rectángulo/Círculo)

---
+++ StickerBoard/StickerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StickerBoard
+{
+    public class StickerFactory
+    {
+        public enum TipoSticker
+        {
+            Circulo,
+            Rectangulo
+        }
+        public static Sticker CrearSticker(TipoSticker tipo, int posX, int posY, int tamaño, System.Drawing.Color color)
+        {
+            switch (tipo)
+            {
+                case TipoSticker.Circulo:
+                    return new StickerCirculo(posX, posY, tamaño, color);
+                case TipoSticker.Rectangulo:
+                    return new StickerRectangulo(posX, posY, tamaño, color);
+                default:
+                    throw new ArgumentException("Tipo de sticker no válido");
+            }
+        }
+    }
+}
