f3c1583641b59d2a7860e523d55727c935319bfc
thetrueseros <leugimdzn@hotmail.com>
2025-10-18 00:01:34 -0500

    feat: agrega clase base abstracta Sticker

--- StickerBoard/Sticker.cs
+++ StickerBoard/Sticker.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace StickerBoard
 {
     public abstract class Sticker
     {
-        private int posX { get; set; }
-        private int posY { get; set; }
-        private int tamaño { get; set; }
-        public abstract void Dibujar( int posX, int posY, int tamaño );
+        // Propiedades protegidas accesibles por las clases derivadas
+        protected int posX { get; set; }
+        protected int posY { get; set; }
+        protected int tamaño { get; set; }
+        protected Color color { get; set; }
+
+        // Propiedades públicas de sólo lectura (útiles para la interfaz / Factory)
+        public int PosX => posX;
+        public int PosY => posY;
+        public int Tamaño => tamaño;
+        public Color Color => color;
+
+        // Constructor principal usado por Factory
+        protected Sticker(int posX, int posY, int tamaño, Color color)
+        {
+            if (tamaño <= 0) throw new ArgumentOutOfRangeException(nameof(tamaño), "El tamaño debe ser mayor que cero.");
+            this.posX = posX;
+            this.posY = posY;
+            this.tamaño = tamaño;
+            this.color = color;
+        }
+
+        // Helper: rectángulo que ocupa el sticker.
+        // Aquí se decide que posX,posY son el centro de la figura.
+        protected Rectangle GetBounds()
+        {
+            int half = tamaño / 2;
+            return new Rectangle(posX - half, posY - half, tamaño, tamaño);
+        }
+
+        // Cada subclase debe implementar cómo se dibuja usando Graphics
+        public abstract void Dibujar(Graphics g);
     }
 }
